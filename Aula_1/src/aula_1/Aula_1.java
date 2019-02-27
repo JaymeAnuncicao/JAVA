e8a0b5335f3135df81d2790220d927b93a7a6f6f
JaymeAnuncicao <43611622+JaymeAnuncicao@users.noreply.github.com>
2019-02-26 21:05:44 -0300

    Add files via upload

---
+++ Aula_1/src/aula_1/Aula_1.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula_1;
+import java.util.Scanner;
+/**
+ *
+ * @author aluno
+ */
+public class Aula_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        
+       /* System.out.println("digite o numero de notas");
+        int n = ler.nextInt();
+        int[] nota = new int[n];
+        int soma = 0;
+        float media;
+        for(int i=0;i<n;i++){
+            System.out.println("Digite a nota "+(i+1));
+            nota[i] = ler.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            soma = soma+nota[i];
+        }
+        media = soma/n;
+        
+        System.out.println("A media é "+media);*/
+        /*System.out.println("Informe o número");
+        int n = ler.nextInt();
+        int conf,soma=0,div;
+        for(int i=1;i<n;i++){
+            div = n%i;
+            //System.out.println(div);
+            if(div == 0){
+                soma = soma+i;
+            }
+        }
+        if(soma == n){
+            System.out.println("é perfeito");
+        }else{System.out.println("se fudeu!");}*/
+        int n;
+        
+        do{
+            
+            
+        }while(n !=-1);
+    }
+}
