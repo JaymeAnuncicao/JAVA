c43788fc05ac38cef01dd9dc1665969462119405
JaymeAnuncicao <43611622+JaymeAnuncicao@users.noreply.github.com>
2019-03-26 21:21:50 -0300

    Create Teste.java

---
+++ Aula3/src/Teste.java
@@ -0,0 +1,19 @@
+
+package teste;
+
+
+public class Teste {
+
+
+    public static void main(String[] args) {
+        Unipraxis aluno1 = new Unipraxis();
+        
+        aluno1.setNome("jayme");
+        
+        aluno1.setAno(2006);
+        
+        System.out.println(aluno1.getPermanecido());
+        
+    }
+    
+}
