ce72f67e51ea6214bc3d12d4028fbcb75f98330e
JaymeAnuncicao <43611622+JaymeAnuncicao@users.noreply.github.com>
2019-03-29 09:41:40 -0300

    Update Teste.java

--- Aula3/src/Teste.java
+++ Aula3/src/Teste.java
@@ -1,18 +1,31 @@
 
 package teste;
 
+import java.util.Random;
+
 
 public class Teste {
 
-
+   
     public static void main(String[] args) {
-        Unipraxis aluno1 = new Unipraxis();
+       Unipraxis aluno1 = new Unipraxis();
+//      Graduando ou pós-graduando ?
+            aluno1.setGraduacao("pós-graduando");
+//      Come do aluno
+            aluno1.setNome("jayme");
+//      Curso do aluno  
+            aluno1.setCurso("Engenharia da Computação");
+//      Matricula do aluno 
+            aluno1.setMatricula(144181032);
+//      Diciplinas aprovadas do aluno
+            aluno1.setDiciplinas(8);
+//      Mensalidade do aluno
+            aluno1.setMensalidade(1920.35F);
+//      Ano de Ingresso do aluno
+            aluno1.setAno(2017);
         
-        aluno1.setNome("jayme");
-        
-        aluno1.setAno(2006);
-        
-        System.out.println(aluno1.getPermanecido());
+//        System.out.println(aluno1.getPermanecido()+" anos");
+        aluno1.Status();
         
     }
     
